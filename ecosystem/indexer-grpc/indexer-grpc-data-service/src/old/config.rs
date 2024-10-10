ca48092b2bdacb6f6b68f083ac696ebc8bd13f15
Guoteng Rao <3603304+grao1991@users.noreply.github.com>
2024-10-10 00:16:38 +0000

    big change

--- ecosystem/indexer-grpc/indexer-grpc-data-service/src/old/config.rs
+++ ecosystem/indexer-grpc/indexer-grpc-data-service/src/old/config.rs
@@ -1,16 +1,12 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{
-    connection_manager::ConnectionManager,
-    historical_data_service::HistoricalDataService,
-    live_data_service::LiveDataService,
-    service::{DataServiceWrapper, DataServiceWrapperWrapper},
-};
+use crate::service::RawDataServerWrapper;
 use anyhow::{bail, Result};
 use aptos_indexer_grpc_server_framework::RunnableConfig;
 use aptos_indexer_grpc_utils::{
-    config::IndexerGrpcFileStoreConfig, in_memory_cache::InMemoryCacheConfig,
+    compression_util::StorageFormat, config::IndexerGrpcFileStoreConfig,
+    in_memory_cache::InMemoryCacheConfig, types::RedisUrl,
 };
 use aptos_protos::{
     indexer::v1::FILE_DESCRIPTOR_SET as INDEXER_V1_FILE_DESCRIPTOR_SET,
@@ -18,16 +14,11 @@ use aptos_protos::{
     util::timestamp::FILE_DESCRIPTOR_SET as UTIL_TIMESTAMP_FILE_DESCRIPTOR_SET,
 };
 use aptos_transaction_filter::BooleanTransactionFilter;
-use once_cell::sync::OnceCell;
 use serde::{Deserialize, Serialize};
 use std::{net::SocketAddr, sync::Arc};
-use tokio::task::JoinHandle;
 use tonic::{codec::CompressionEncoding, transport::Server};
-use tracing::info;
 
 pub const SERVER_NAME: &str = "idxdatasvc";
-
-pub(crate) const MAX_MESSAGE_SIZE: usize = 256 * (1 << 20);
 
 // Default max response channel size.
 const DEFAULT_MAX_RESPONSE_CHANNEL_SIZE: usize = 3;
@@ -37,9 +28,6 @@ const DEFAULT_MAX_RESPONSE_CHANNEL_SIZE: usize = 3;
 // tonic server: https://docs.rs/tonic/latest/tonic/transport/server/struct.Server.html#method.http2_keepalive_interval
 const HTTP2_PING_INTERVAL_DURATION: std::time::Duration = std::time::Duration::from_secs(60);
 const HTTP2_PING_TIMEOUT_DURATION: std::time::Duration = std::time::Duration::from_secs(10);
-
-static LIVE_DATA_SERVICE: OnceCell<LiveDataService<'static>> = OnceCell::new();
-static HISTORICAL_DATA_SERVICE: OnceCell<HistoricalDataService> = OnceCell::new();
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
@@ -59,35 +47,7 @@ pub struct NonTlsConfig {
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-pub struct LiveDataServiceConfig {
-    pub enabled: bool,
-    #[serde(default = "LiveDataServiceConfig::default_num_slots")]
-    pub num_slots: usize,
-    #[serde(default = "LiveDataServiceConfig::default_size_limit_bytes")]
-    pub size_limit_bytes: usize,
-}
-
-impl LiveDataServiceConfig {
-    fn default_num_slots() -> usize {
-        200000000
-    }
-
-    fn default_size_limit_bytes() -> usize {
-        10000000000
-    }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(deny_unknown_fields)]
-pub struct HistoricalDataServiceConfig {
-    pub enabled: bool,
-    pub file_store_config: IndexerGrpcFileStoreConfig,
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-#[serde(deny_unknown_fields)]
 pub struct IndexerGrpcDataServiceConfig {
-    pub chain_id: u64,
     /// If given, we will run a server that uses TLS.
     pub data_service_grpc_tls_config: Option<TlsConfig>,
     /// If given, we will run a server that does not use TLS.
@@ -95,6 +55,19 @@ pub struct IndexerGrpcDataServiceConfig {
     /// The size of the response channel that response can be buffered.
     #[serde(default = "IndexerGrpcDataServiceConfig::default_data_service_response_channel_size")]
     pub data_service_response_channel_size: usize,
+    /// Deprecated: a list of auth tokens that are allowed to access the service.
+    #[serde(default)]
+    pub whitelisted_auth_tokens: Vec<String>,
+    /// Deprecated: if set, don't check for auth tokens.
+    #[serde(default)]
+    pub disable_auth_check: bool,
+    /// File store config.
+    pub file_store_config: IndexerGrpcFileStoreConfig,
+    /// Redis read replica address.
+    pub redis_read_replica_address: RedisUrl,
+    /// Support compressed cache data.
+    #[serde(default = "IndexerGrpcDataServiceConfig::default_enable_cache_compression")]
+    pub enable_cache_compression: bool,
     #[serde(default)]
     pub in_memory_cache_config: InMemoryCacheConfig,
     /// Any transaction that matches this filter will be stripped. This means we remove
@@ -109,100 +82,46 @@ pub struct IndexerGrpcDataServiceConfig {
     /// separate filters that describe each type of txn we want to strip.
     #[serde(default = "IndexerGrpcDataServiceConfig::default_txns_to_strip_filter")]
     pub txns_to_strip_filter: BooleanTransactionFilter,
-
-    pub live_data_service_config: LiveDataServiceConfig,
-    pub historical_data_service_config: HistoricalDataServiceConfig,
-    pub grpc_manager_addresses: Vec<String>,
-    pub self_advertised_address: String,
 }
 
 impl IndexerGrpcDataServiceConfig {
+    pub fn new(
+        data_service_grpc_tls_config: Option<TlsConfig>,
+        data_service_grpc_non_tls_config: Option<NonTlsConfig>,
+        data_service_response_channel_size: Option<usize>,
+        disable_auth_check: bool,
+        file_store_config: IndexerGrpcFileStoreConfig,
+        redis_read_replica_address: RedisUrl,
+        enable_cache_compression: bool,
+        in_memory_cache_config: InMemoryCacheConfig,
+        txns_to_strip_filter: BooleanTransactionFilter,
+    ) -> Self {
+        Self {
+            data_service_grpc_tls_config,
+            data_service_grpc_non_tls_config,
+            data_service_response_channel_size: data_service_response_channel_size
+                .unwrap_or_else(Self::default_data_service_response_channel_size),
+            whitelisted_auth_tokens: vec![],
+            disable_auth_check,
+            file_store_config,
+            redis_read_replica_address,
+            enable_cache_compression,
+            in_memory_cache_config,
+            txns_to_strip_filter,
+        }
+    }
+
     pub const fn default_data_service_response_channel_size() -> usize {
         DEFAULT_MAX_RESPONSE_CHANNEL_SIZE
+    }
+
+    pub const fn default_enable_cache_compression() -> bool {
+        false
     }
 
     pub fn default_txns_to_strip_filter() -> BooleanTransactionFilter {
         // This filter matches no txns.
         BooleanTransactionFilter::new_or(vec![])
-    }
-
-    async fn create_live_data_service(
-        &self,
-        tasks: &mut Vec<JoinHandle<Result<()>>>,
-    ) -> Option<DataServiceWrapper> {
-        if !self.live_data_service_config.enabled {
-            return None;
-        }
-        let connection_manager = Arc::new(
-            ConnectionManager::new(
-                self.grpc_manager_addresses.clone(),
-                self.self_advertised_address.clone(),
-                /*is_live_data_service=*/ true,
-            )
-            .await,
-        );
-        let (handler_tx, handler_rx) = tokio::sync::mpsc::channel(10);
-        let service = DataServiceWrapper::new(
-            connection_manager.clone(),
-            handler_tx,
-            self.data_service_response_channel_size,
-        );
-
-        let connection_manager_clone = connection_manager.clone();
-        tasks.push(tokio::task::spawn(async move {
-            connection_manager_clone.start().await;
-            Ok(())
-        }));
-
-        let config = self.live_data_service_config.clone();
-        tasks.push(tokio::task::spawn_blocking(move || {
-            LIVE_DATA_SERVICE
-                .get_or_init(|| LiveDataService::new(config, connection_manager))
-                .run(handler_rx);
-            Ok(())
-        }));
-
-        Some(service)
-    }
-
-    async fn create_historical_data_service(
-        &self,
-        tasks: &mut Vec<JoinHandle<Result<()>>>,
-    ) -> Option<DataServiceWrapper> {
-        if !self.historical_data_service_config.enabled {
-            return None;
-        }
-        let connection_manager = Arc::new(
-            ConnectionManager::new(
-                self.grpc_manager_addresses.clone(),
-                self.self_advertised_address.clone(),
-                /*is_live_data_service=*/ false,
-            )
-            .await,
-        );
-        let (handler_tx, handler_rx) = tokio::sync::mpsc::channel(10);
-        let service = DataServiceWrapper::new(
-            connection_manager.clone(),
-            handler_tx,
-            self.data_service_response_channel_size,
-        );
-
-        let connection_manager_clone = connection_manager.clone();
-        tasks.push(tokio::task::spawn(async move {
-            connection_manager_clone.start().await;
-            Ok(())
-        }));
-
-        let chain_id = self.chain_id;
-        let config = self.historical_data_service_config.clone();
-        tasks.push(tokio::task::spawn_blocking(move || {
-            HISTORICAL_DATA_SERVICE
-                .get_or_init(|| HistoricalDataService::new(chain_id, config, connection_manager))
-                .run(handler_rx);
-            Ok(())
-        }));
-
-        Some(service)
     }
 }
 
@@ -228,42 +147,57 @@ impl RunnableConfig for IndexerGrpcDataServiceConfig {
             .register_encoded_file_descriptor_set(INDEXER_V1_FILE_DESCRIPTOR_SET)
             .register_encoded_file_descriptor_set(TRANSACTION_V1_TESTING_FILE_DESCRIPTOR_SET)
             .register_encoded_file_descriptor_set(UTIL_TIMESTAMP_FILE_DESCRIPTOR_SET)
-            .build_v1()
+            .build()
             .map_err(|e| anyhow::anyhow!("Failed to build reflection service: {}", e))?
             .send_compressed(CompressionEncoding::Zstd)
             .accept_compressed(CompressionEncoding::Zstd)
             .accept_compressed(CompressionEncoding::Gzip);
 
-        let mut tasks = vec![];
+        let cache_storage_format: StorageFormat = if self.enable_cache_compression {
+            StorageFormat::Lz4CompressedProto
+        } else {
+            StorageFormat::Base64UncompressedProto
+        };
 
-        let live_data_service = self.create_live_data_service(&mut tasks).await;
-        let historical_data_service = self.create_historical_data_service(&mut tasks).await;
+        println!(
+            ">>>> Starting Redis connection: {:?}",
+            &self.redis_read_replica_address.0
+        );
+        let redis_conn = redis::Client::open(self.redis_read_replica_address.0.clone())?
+            .get_tokio_connection_manager()
+            .await?;
+        println!(">>>> Redis connection established");
+        // InMemoryCache.
+        let in_memory_cache =
+            aptos_indexer_grpc_utils::in_memory_cache::InMemoryCache::new_with_redis_connection(
+                self.in_memory_cache_config.clone(),
+                redis_conn,
+                cache_storage_format,
+            )
+            .await?;
+        println!(">>>> InMemoryCache established");
+        // Add authentication interceptor.
+        let server = RawDataServerWrapper::new(
+            self.redis_read_replica_address.clone(),
+            self.file_store_config.clone(),
+            self.data_service_response_channel_size,
+            self.txns_to_strip_filter.clone(),
+            cache_storage_format,
+            Arc::new(in_memory_cache),
+        )?;
+        let svc = aptos_protos::indexer::v1::raw_data_server::RawDataServer::new(server)
+            .send_compressed(CompressionEncoding::Zstd)
+            .accept_compressed(CompressionEncoding::Zstd)
+            .accept_compressed(CompressionEncoding::Gzip);
+        println!(">>>> Starting gRPC server: {:?}", &svc);
 
-        let wrapper = Arc::new(DataServiceWrapperWrapper::new(
-            live_data_service,
-            historical_data_service,
-        ));
-        let wrapper_service_raw =
-            aptos_protos::indexer::v1::raw_data_server::RawDataServer::from_arc(wrapper.clone())
-                .send_compressed(CompressionEncoding::Zstd)
-                .accept_compressed(CompressionEncoding::Zstd)
-                .accept_compressed(CompressionEncoding::Gzip)
-                .max_decoding_message_size(MAX_MESSAGE_SIZE)
-                .max_encoding_message_size(MAX_MESSAGE_SIZE);
-        let wrapper_service =
-            aptos_protos::indexer::v1::data_service_server::DataServiceServer::from_arc(wrapper)
-                .send_compressed(CompressionEncoding::Zstd)
-                .accept_compressed(CompressionEncoding::Zstd)
-                .accept_compressed(CompressionEncoding::Gzip)
-                .max_decoding_message_size(MAX_MESSAGE_SIZE)
-                .max_encoding_message_size(MAX_MESSAGE_SIZE);
-        let wrapper_service_raw_clone = wrapper_service_raw.clone();
-        let wrapper_service_clone = wrapper_service.clone();
+        let svc_clone = svc.clone();
         let reflection_service_clone = reflection_service.clone();
 
+        let mut tasks = vec![];
         if let Some(config) = &self.data_service_grpc_non_tls_config {
             let listen_address = config.data_service_grpc_listen_address;
-            info!(
+            tracing::info!(
                 grpc_address = listen_address.to_string().as_str(),
                 "[data service] starting gRPC server with non-TLS."
             );
@@ -271,8 +205,7 @@ impl RunnableConfig for IndexerGrpcDataServiceConfig {
                 Server::builder()
                     .http2_keepalive_interval(Some(HTTP2_PING_INTERVAL_DURATION))
                     .http2_keepalive_timeout(Some(HTTP2_PING_TIMEOUT_DURATION))
-                    .add_service(wrapper_service_clone)
-                    .add_service(wrapper_service_raw_clone)
+                    .add_service(svc_clone)
                     .add_service(reflection_service_clone)
                     .serve(listen_address)
                     .await
@@ -284,7 +217,7 @@ impl RunnableConfig for IndexerGrpcDataServiceConfig {
             let cert = tokio::fs::read(config.cert_path.clone()).await?;
             let key = tokio::fs::read(config.key_path.clone()).await?;
             let identity = tonic::transport::Identity::from_pem(cert, key);
-            info!(
+            tracing::info!(
                 grpc_address = listen_address.to_string().as_str(),
                 "[Data Service] Starting gRPC server with TLS."
             );
@@ -293,8 +226,7 @@ impl RunnableConfig for IndexerGrpcDataServiceConfig {
                     .http2_keepalive_interval(Some(HTTP2_PING_INTERVAL_DURATION))
                     .http2_keepalive_timeout(Some(HTTP2_PING_TIMEOUT_DURATION))
                     .tls_config(tonic::transport::ServerTlsConfig::new().identity(identity))?
-                    .add_service(wrapper_service)
-                    .add_service(wrapper_service_raw)
+                    .add_service(svc)
                     .add_service(reflection_service)
                     .serve(listen_address)
                     .await
--- ecosystem/indexer-grpc/indexer-grpc-file-store/src/lib.rs
+++ ecosystem/indexer-grpc/indexer-grpc-file-store/src/lib.rs
@@ -1,67 +1,126 @@
 // Copyright © Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
 
+pub mod data_manager;
+pub mod file_store_uploader;
+pub mod metadata_manager;
 pub mod metrics;
-pub mod processor;
+pub mod service;
 
+use crate::{
+    data_manager::DataManager, metadata_manager::MetadataManager, service::GrpcManagerService,
+};
 use anyhow::Result;
 use aptos_indexer_grpc_server_framework::RunnableConfig;
-use aptos_indexer_grpc_utils::{config::IndexerGrpcFileStoreConfig, types::RedisUrl};
-use processor::Processor;
+use aptos_indexer_grpc_utils::config::IndexerGrpcFileStoreConfig;
+use aptos_protos::indexer::v1::grpc_manager_server::GrpcManagerServer;
+use file_store_uploader::FileStoreUploader;
 use serde::{Deserialize, Serialize};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+use tokio::sync::Mutex;
+use tonic::{codec::CompressionEncoding, transport::Server};
+
+const HTTP2_PING_INTERVAL_DURATION: Duration = Duration::from_secs(60);
+const HTTP2_PING_TIMEOUT_DURATION: Duration = Duration::from_secs(10);
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct ServiceConfig {
+    listen_address: SocketAddr,
+}
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
-pub struct IndexerGrpcFileStoreWorkerConfig {
-    pub file_store_config: IndexerGrpcFileStoreConfig,
-    pub redis_main_instance_address: RedisUrl,
-    pub enable_expensive_logging: Option<bool>,
-    pub chain_id: u64,
-    #[serde(default = "default_enable_cache_compression")]
-    pub enable_cache_compression: bool,
-}
-
-const fn default_enable_cache_compression() -> bool {
-    false
-}
-
-impl IndexerGrpcFileStoreWorkerConfig {
-    pub fn new(
-        file_store_config: IndexerGrpcFileStoreConfig,
-        redis_main_instance_address: RedisUrl,
-        enable_expensive_logging: Option<bool>,
-        chain_id: u64,
-        enable_cache_compression: bool,
-    ) -> Self {
-        Self {
-            file_store_config,
-            redis_main_instance_address,
-            enable_expensive_logging,
-            chain_id,
-            enable_cache_compression,
-        }
-    }
+pub struct IndexerGrpcManagerConfig {
+    chain_id: u64,
+    service_config: ServiceConfig,
+    file_store_config: IndexerGrpcFileStoreConfig,
+    self_advertised_address: String,
+    grpc_manager_addresses: Vec<String>,
+    fullnode_addresses: Vec<String>,
 }
 
 #[async_trait::async_trait]
-impl RunnableConfig for IndexerGrpcFileStoreWorkerConfig {
+impl RunnableConfig for IndexerGrpcManagerConfig {
     async fn run(&self) -> Result<()> {
-        let mut processor = Processor::new(
-            self.redis_main_instance_address.clone(),
-            self.file_store_config.clone(),
-            self.chain_id,
-            self.enable_cache_compression,
-        )
-        .await
-        .expect("Failed to create file store processor");
-        processor
-            .run()
-            .await
-            .expect("File store processor exited unexpectedly");
+        GrpcManager::new(self).await.start(&self.service_config);
+
         Ok(())
     }
 
     fn get_server_name(&self) -> String {
-        "idxfilestore".to_string()
+        "grpc_manager".to_string()
+    }
+}
+
+struct GrpcManager {
+    chain_id: u64,
+    filestore_uploader: Mutex<FileStoreUploader>,
+    metadata_manager: Arc<MetadataManager>,
+    data_manager: Arc<DataManager>,
+}
+
+impl GrpcManager {
+    pub(crate) async fn new(config: &IndexerGrpcManagerConfig) -> Self {
+        let chain_id = config.chain_id;
+        let filestore_uploader = Mutex::new(
+            FileStoreUploader::new(chain_id, config.file_store_config.clone())
+                .await
+                .expect(&format!(
+                    "Failed to create filestore uploader, config: {:?}.",
+                    config.file_store_config
+                )),
+        );
+        let metadata_manager = Arc::new(MetadataManager::new(
+            config.self_advertised_address.clone(),
+            config.grpc_manager_addresses.clone(),
+            config.fullnode_addresses.clone(),
+        ));
+        let data_manager = Arc::new(
+            DataManager::new(
+                chain_id,
+                config.file_store_config.clone(),
+                filestore_uploader.lock().await.version(),
+                metadata_manager.clone(),
+            )
+            .await,
+        );
+        Self {
+            chain_id,
+            filestore_uploader,
+            metadata_manager,
+            data_manager,
+        }
+    }
+
+    pub(crate) fn start(&self, service_config: &ServiceConfig) {
+        let service = GrpcManagerServer::new(GrpcManagerService::new(
+            self.chain_id,
+            self.metadata_manager.clone(),
+            self.data_manager.clone(),
+        ))
+        .send_compressed(CompressionEncoding::Zstd)
+        .accept_compressed(CompressionEncoding::Zstd);
+        let server = Server::builder()
+            .http2_keepalive_interval(Some(HTTP2_PING_INTERVAL_DURATION))
+            .http2_keepalive_timeout(Some(HTTP2_PING_TIMEOUT_DURATION))
+            .add_service(service);
+
+        tokio_scoped::scope(|s| {
+            s.spawn(async move {
+                self.metadata_manager.start().await.unwrap();
+            });
+            s.spawn(async move { self.data_manager.start().await });
+            s.spawn(async move {
+                self.filestore_uploader
+                    .lock()
+                    .await
+                    .start(self.data_manager.clone())
+                    .await
+                    .unwrap();
+            });
+            s.spawn(async move {
+                server.serve(service_config.listen_address).await.unwrap();
+            });
+        });
     }
 }
