7bf22d022bbfee0370fd363cbe6c0c0aaae86f8d
Brian Cho <brian@aptoslabs.com>
2024-11-06 16:27:53 -0800

    Execution backpressure: reduce target 250 -> 200ms

--- config/src/config/consensus_config.rs
+++ config/src/config/consensus_config.rs
@@ -197,7 +197,7 @@ impl Default for ConsensusConfig {
                 num_blocks_to_look_at: 12,
                 min_blocks_to_activate: 4,
                 percentile: 0.5,
-                target_block_time_ms: 250,
+                target_block_time_ms: 200,
                 min_block_time_ms_to_activate: 100,
                 // TODO: appropriate value?
                 min_calibrated_block_gas_limit: 1000,
